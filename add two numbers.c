f63de9e97784d647884ed6aa51df722358a4e084
Nmvsscharan <nmvsscharan@gmail.com>
2021-03-11 11:15:35 +0530

    adding numbes

---
+++ add two numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,sum=0;
+	printf("enter two values a,b:");
+	scanf("%d%d",&a,&b);
+	sum=a+b;
+	printf("sum of two numbers is sum=%d",sum);
+	return 0;
+	
+	
+	
+	
+	
+}
