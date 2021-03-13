29fb79daa1ae081f4843d4edcc13140429bc59b9
Nmvsscharan <nmvsscharan@gmail.com>
2021-03-13 22:49:02 +0530

    mul

--- mul of numbers.c
+++ mul of numbers.c
@@ -2,10 +2,12 @@
 int main()
 {
 	int a,b,mul;
-	printf("enter the values of a,b:");
+	printf("enter the values a,b:");
 	scanf("%d%d",&a,&b);
 	mul=a*b;
-	printf("%d=%d*%d",mul,a,b);
+	printf("%d",mul);
+	
+	
 	
 	
 	
